bd19839fe31abe315aac7f43438abbe4a2a8bfd7
hoquocnam92 <hoquocnam92@gmail.com>
2026-08-01 15:40:00 +0700

    chore: add package directories

--- apps/api/Modules/Borrowings/DTOs/RenewRequestDto.cs
+++
@@ -1,8 +0,0 @@
-namespace api.Modules.Borrowings.DTOs
-{
-    public class RenewRequestDto
-    {
-        public int ExtraDays { get; set; } = 7; // Mặc định gia hạn thêm 7 ngày
-        public string? Note { get; set; }
-    }
-}
